2809ae05da65422cee2c4487ffd91b13b89dcfb5
muhammadatiq <muhammadatiq757@gmail.com>
2025-04-21 12:29:33 +0600

    feat: increase font size of the Home heading

--- frontend/src/pages/public/Home.jsx
+++ frontend/src/pages/public/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
       className="text-center bg-gray-200 text-black p-4 flex flex-col justify-center items-center"
       style={{ minHeight: "calc(100vh - 112px)" }}
     >
-      <p className="text-4xl font-semibold">Home</p>
+      <p className="text-5xl font-semibold">Home</p>
     </div>
   );
 };
