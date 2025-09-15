62f6d7e9c04542de1075e7041f53e8510e6ece90
Rakib Hossen <149133972+RakibHossen678@users.noreply.github.com>
2025-09-15 15:18:19 +0600

    Refactor auth model by removing unused fields

--- backend/src/modules/auth/auth.model.js
+++ backend/src/modules/auth/auth.model.js
@@ -7,36 +7,6 @@ const UserSchema = new mongoose.Schema(
     password: { type: String, default: null },
     googleId: { type: String, default: null, unique: true },
     role: { type: String, enum: ["user", "admin"], default: "user" },
-    country: { type: String, default: null },
-    phone: { type: String, default: null },
-    subscriptions: [
-      {
-        planId: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "SubscriptionPlan",
-          required: true,
-        },
-        status: {
-          type: String,
-          enum: ["active", "expired"],
-          default: "expired",
-        },
-        startDate: { type: Date, required: true },
-        endDate: { type: Date, required: true },
-        renewalCount: { type: Number, default: 0 },
-      },
-    ],
-
-    currentSubscription: {
-      planId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "SubscriptionPlan",
-        default: null,
-      },
-      status: { type: String, enum: ["active", "expired"], default: "expired" },
-      startDate: { type: Date, default: null },
-      endDate: { type: Date, default: null },
-    },
   },
   { timestamps: true }
 );
